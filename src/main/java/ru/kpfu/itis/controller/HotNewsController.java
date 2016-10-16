9e8ee44ecf85f449026b7da084830db991a5ed46
Melnikov Semen <sema.melnikov@gmail.com>
2016-10-16 11:30:21 +0300

    Add modal

--- src/main/java/ru/kpfu/itis/controller/HotNewsController.java
+++ src/main/java/ru/kpfu/itis/controller/HotNewsController.java
@@ -3,13 +3,25 @@ package ru.kpfu.itis.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.kpfu.itis.news.News;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class HotNewsController {
 
     @RequestMapping("/")
-    public String[] newsPrint() {
-        String[] news = new String[] {"Dog1" , "Dog2", "Dog3", "Lemon?", "Cats don't like cold water"};
+    public News[] newsPrint() {
+        News[] news = new News[10];
+        News news0 = new News();
+        news0.setTitle("First news class");
+        news0.setText("My first test");
+        news0.setAuthor("Sema");
+        news0.setDate(LocalDateTime.now());
+        news0.setMinus(5);
+        news0.setPlus(0);
+        news0.setId(131);
+        news[0] = news0;
         return news;
     }
 }
