cd3b08af9395615d9244b2d030271b8eac78b865
Melnikov Semen <sema.melnikov@gmail.com>
2016-10-16 11:40:00 +0300

    Rename package

--- src/main/java/ru/kpfu/itis/controller/HotNewsController.java
+++ src/main/java/ru/kpfu/itis/controller/HotNewsController.java
@@ -3,7 +3,7 @@ package ru.kpfu.itis.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.kpfu.itis.news.News;
+import ru.kpfu.itis.modal.News;
 
 import java.time.LocalDateTime;
 
@@ -14,7 +14,7 @@ public class HotNewsController {
     public News[] newsPrint() {
         News[] news = new News[10];
         News news0 = new News();
-        news0.setTitle("First news class");
+        news0.setTitle("First modal class");
         news0.setText("My first test");
         news0.setAuthor("Sema");
         news0.setDate(LocalDateTime.now());
