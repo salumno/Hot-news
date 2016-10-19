8dabc1b61c63f8be5adb7ac2d68136db1525e5ba
Melnikov Semen <sema.melnikov@gmail.com>
2016-10-19 23:58:20 +0300

    Add fetching data from the server

--- frontend/scripts/react.js
+++ frontend/scripts/react.js
@@ -10,23 +10,50 @@ var data = [
 ];
 
 var HotNewsBox = React.createClass({ // Создаем лишь часть большой структуры. React знает, как ей управлять.
+
+    loadNewsFromServer: function () { // Первое получение обновлений* и дальнейшее из получение и обработка раз в 2 секунды
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    getInitialState: function () {
+        return {data: []};  // Установка-закрепление изначального состояния data
+    },
+
+    componentDidMount: function () {
+        this.loadNewsFromServer();
+        setInterval(this.loadNewsFromServer, this.props.pollInterval); // Установка временного интервала между запросами
+    },
+
     render: function () {
         return (
             <div className="hotNewsBox">
                 Hello, world! Imma HotNewsBox!
                 <h1>HotNews</h1>  /*Оболочка познакомилась со своими компонентами*/
-                <HotNewsList data={this.props.data}/> /*Положили в список наши данные?*/
-                <HotNewsForm /> /*в ячейку HotNewsList'a data положили значение data-массива*/
+                <HotNewsList data={this.state.data}/> /*Берем текущее состояние data/
+                <HotNewsForm /> /**/
             </div>
         );
     }
+
 });
 
 var HotNewsList = React.createClass({
+
     render: function () { /*Массив получаем с помощью this.props.data*/
+
         var newsNode = this.props.data.map(function (hotNews) { /*Получаем новый массив, состоящий из React элементов*/
             return (
-                <HotNews author={hotNews.author} key={hotNews.id}> /*???*/
+                <HotNews author={hotNews.author} key={hotNews.id}> /*??? TODO*/
                     {hotNews.text}
                  </HotNews>
             );
@@ -39,9 +66,11 @@ var HotNewsList = React.createClass({
             </div>
         );
     }
+
 });
 
 var HotNewsForm = React.createClass({
+
     render: function () {
         return (
             <div className="hotNewsForm">
@@ -49,23 +78,26 @@ var HotNewsForm = React.createClass({
             </div>
         );
     }
+
 });
 // Создали "внутренности/скелет" нашего бокса.
 
 var HotNews = React.createClass({
+
     render: function () {
         return (
             <div className="hotNews">
                 <h2 className="newsAuthor">
-                    {this.props.author} /*Доступ к автору*/
+                    {this.props.author} /*Возьмет то, что мы передадим ему в автора? Родитель HotNewsList даст*/
                 </h2>
-                {this.props.children} /*Доступ к вложенным элементам*/
+                {this.props.children} /*Возьмет то, что будет во вложенной фигне*/
             </div>
         );
     }
+
 });
 
 ReactDOM.render( // Всегда внизу, запускаем только когда все компоненты определены.
-    <HotNewsBox data={data} />, // Рендерим оболочку всей нашей структуры.
+    <HotNewsBox url="/api/news" pollInterval={2000} />, // А вот и те данные (массив data, заданный на самом верху) которые пойдут в this.props.data в HotNewsList через HotNewsBox
     document.getElementsById('content')
 );
